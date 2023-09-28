5b07d6beb4e1b263f1ebade9609905120c0498c2
Meg Roth <megroth2@gmail.com>
2023-09-28 14:04:13 -0600

    add comparisons answers

--- comparisons.rb
+++ comparisons.rb
@@ -22,24 +22,31 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # this should print: "Is numberTeachers greater than numberStudents?" false
 
 # YOU DO: print the result of the comparison: is number_teachers less than number_students?
+puts "Is number_teachers less than number_students?", number_teachers < number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_teachers equal to string_teachers?
+puts "Is number_teachers equal to number_students?", number_teachers == number_students
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_teachers not equal to number_students?
+puts "Is number_teachers not equal to number_students?", number_teachers != number_students
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 20?
+puts "Is number_students greater than or equal to 20?", number_students >= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students greater than or equal to 21?
+puts "Is number_students greater than or equal to 21?", number_students >= 21
 # this should print: false
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 20?
+puts "Is number_students less than or equal to 20?", number_students <= 20
 # this should print: true
 
 # YOU DO: print the result of the comparison: is number_students less than or equal to 21?
+puts "Is number_students less than or equal to 21?", number_students <= 21
 # this should print: true
 
 
@@ -54,20 +61,24 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 
 puts 4 < 9
 #YOU DO: Explain.
+puts "Line 62 (above) evaluates whether 4 is less than 9 and displays a boolean value (above). Since 4 is less than 9, it evaluates to true."
 
 books = 3
 puts 4 < books
 # YOU DO: Explain.
+puts "Line 66 assigns an integer value of '3' to a 'books' variable. Line 67 evaluates if 4 is less than books and displays a boolean value (above). Since books is 3 and 4 is NOT less than books, it evaluates to false."
 
 friends = 6
 siblings = 2
 puts friends > siblings
 # YOU DO: Explain.
+puts "Line 71 assigns an integer value of '6' to a 'friends' variable. Line 72 assigns an integer value of '2' to a 'siblings' variable. Line 73 evaluates if friends is greater than siblings and displays a boolean value (above). Since friends is 6 and siblings is 2, friends is greater than siblings, so it evaluates to true."
 
 attendees = 9
 meals = 8
 puts attendees != meals
 # YOU DO: Explain.
+puts "Line 77 assigns an integer value of '9' to an 'attendees' variable. Line 78 assigns an integer value of '8' to a 'meals' variable. Line 79 evaluates if attendees is not equal to meals and displays a boolean value (above). Since attendees is 9 and meals is 8, attendees does not equal meals, so it evaluates to true."
 
 
 #-------------------
@@ -90,15 +101,17 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+puts loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+puts loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+puts loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
+puts loves_to_play && age <= 2
 
 # What did your final line of code evaluate to? Why do you think that is? Explain.
 # ANSWER:
+puts "My final line of code evaluated to true because the age of the puppy is less than or equal to '2'."
